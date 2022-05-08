68dc85cbf4c13dc64059c4504ba645a9db35fba7
jtorbett23 <josh_torbett@hotmail.co.uk>
2022-05-08 14:09:08 +0100

    show music volume via % not fraction

--- Scripts/SettingsMenu.gd
+++ Scripts/SettingsMenu.gd
@@ -4,10 +4,9 @@ onready var options = $Options
 onready var main_menu = load("res://Scenes/MainMenu.tscn")
 
 func _ready():
-	$"Options/Music Volume".value = AudioManager.current_volume
-	$"Options/Music Volume/Value".text = str(AudioManager.current_volume)
+	$"Options/Music Volume".value = AudioManager.current_volume 
+	$"Options/Music Volume/Value".text = str(AudioManager.current_volume * 100) + "%" 
 	for option in options.get_children():
-		print(str(option.get_class()))
 		match option.get_class():
 			"Button":
 				option.connect("pressed",self, "select_option" ,[option.name])
@@ -15,10 +14,10 @@ func _ready():
 				option.connect("value_changed", self, "handle_slider", [option])
 
 func handle_slider(value, slider):
-	slider.get_node("Value").text = str(value)
 	match slider.name:
 		"Music Volume":
 			AudioManager.set_music_volume(value)
+			slider.get_node("Value").text = str(AudioManager.current_volume * 100) + "%" 
 
 func select_option(action):
 	match action:
